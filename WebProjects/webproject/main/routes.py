d7f6fd3d5cedec9a94007b52e524ee5fca5d46a1
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-07-28 22:25:28 -0700

    Update and rename home.py to routes.py

---
+++ WebProjects/webproject/main/routes.py
@@ -0,0 +1,16 @@
+
+from flask import Blueprint, render_template, request
+
+
+main = Blueprint('main', __name__)
+
+
+@main.route('/')
+@main.route('/home')
+def home():
+	return render_template('home.html')
+
+
+@main.route('/about')
+def about():
+	return render_template('about.html')
