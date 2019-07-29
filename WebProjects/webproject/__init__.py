0052a7d46e546899c7dbcc90a852d2ed3b525afb
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-07-28 22:15:56 -0700

    Update __init__.py

--- WebProjects/webproject/__init__.py
+++ WebProjects/webproject/__init__.py
@@ -1,1 +1,24 @@
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from webproject.config import Config
+
+
+db = SQLAlchemy
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+mail = Mail()
+
+
+def create_app(config_class=Config):
+  app = Flask(__name__)
+  app.config.from_object(Config)
+  
+  db.init_app(app)
+  login_manager.init_app(app)
+  mail.init_app(app)
+  
+  # TODO: import blueprints
